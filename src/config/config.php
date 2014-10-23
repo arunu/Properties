e8c061b92035942f475119149d32cd94cb00b69a
Israel Ortuño <ai.ortuno@gmail.com>
2014-10-23 13:43:47 +0200

    Adapting to Taylors commenting

--- src/config/config.php
+++ src/config/config.php
@@ -8,8 +8,8 @@ return [
 	|--------------------------------------------------------------------------
 	|
 	| If this value is true, all values related to a property will be deleted
-    | if the property is destroyed. Set this to false if you have any database
-    | logic for this task, otherwise you might find some errors when deleting.
+    | if the property is destroyed. Disable if your database handles this
+    | logic for this task, otherwise you may get errors when deleting.
 	|
 	*/
     'delete_cascade' => true
