d150f3f0b65abfde08f6e2780dc205b78d228c32
Israel Ortuño <ai.ortuno@gmail.com>
2014-10-24 02:34:21 +0200

    Clean up

--- src/Devio/Properties/PropertyManager.php
+++ src/Devio/Properties/PropertyManager.php
@@ -1,12 +1,5 @@
 <?php namespace Devio\Properties;
 
-/*
- * This class will be responsible of managing attributes:
- *  - Create new
- *  - Edit existing
- *  - Delete and recursively its values
- *  - List them
- */
 class PropertyManager {
 
     /**
